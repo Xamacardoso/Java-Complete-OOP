fed5f7e5a83cd976131bb4a6a69a57d6487dca3b
Xamacardoso <xcardoso1997@gmail.com>
2024-12-19 21:35:12 -0300

    Add (Basic OOP): Added grades exercise

---
+++ Basic Java/OOP Basics/Grades Exercise/src/application/GradesProgram.java
@@ -0,0 +1,34 @@
+package application;
+
+import entities.Employee;
+import entities.Student;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class GradesProgram {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Student s1 = new Student();
+        s1.name = sc.nextLine();
+        s1.grade1 = sc.nextDouble();
+        s1.grade2 = sc.nextDouble();
+        s1.grade3 = sc.nextDouble();
+
+        double studentFinalGrade = s1.getFinalGrade();
+        System.out.println("Final grade: " + studentFinalGrade);
+
+        if (studentFinalGrade >= 60.0){
+            System.out.println("Pass");
+            return;
+        }
+
+        System.out.println("Failed!");
+        System.out.printf("Missing %.2f points!", 60.0 - studentFinalGrade);
+
+        sc.close();
+    }
+}
---
+++ Basic Java/OOP Basics/Grades Exercise/src/entities/Student.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class Student {
+    public String name;
+    public double grade1, grade2, grade3;
+
+    public double getFinalGrade(){
+        return this.grade1 + this.grade2 + this.grade3;
+    }
+}
